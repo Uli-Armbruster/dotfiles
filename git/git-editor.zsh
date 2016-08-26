49442d32919e6084ef97b21ac1d46ef24b65cc4d
Uli Armbruster <uli.armbruster@heco.de>
2016-08-26 13:33:54 +0200

    added sublime, set notepad++ as my default git editor

--- git/git-editor.zsh
+++ git/git-editor.zsh
@@ -30,8 +30,7 @@ esac
 # to the original file name  after Notepad++ exited. Notepad++ has a custom
 # syntax definition for *.git files:
 # https://gist.github.com/agross/f1bbe96f5b073f20639c2d93d3144ab4
-#
-# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; cp "$file" "$file.git" && /c/Tools/Notepad++/notepad++.exe -multiInst -nosession -noPlugins "$file.git" && mv "$file.git" "$file"'
+export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; cp "$file" "$file.git" && /C/Portable/Apps/PortableApps/Notepad++Portable/App/Notepad++/notepad++.exe -multiInst -nosession -notabbar -noPlugin "$file.git" && mv "$file.git" "$file"'
 
 # ST opens two windows when there is no instance of ST running.
-# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; /c/Tools/Sublime\ Text/subl.exe --wait --new-window "$file" --'
+# export GIT_EDITOR='file="$(cygpath --windows --absolute "$1")"; shift $#; "$(cygpath -u "$dev")/env/dotfiles/tools/Sublime/subl.exe" --wait --new-window "$file" --'
